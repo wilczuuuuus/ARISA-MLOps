eeb183a9ebe1033f829575bf04af1ca1c83008ec
Claus Agerskov <13769591+clausagerskov@users.noreply.github.com>
2025-03-09 10:28:51 +0000

    refroze reqs

--- ARISA_DSML/train.py
+++ ARISA_DSML/train.py
@@ -279,6 +279,7 @@ def get_or_create_experiment(experiment_name:str):
 
 
 if __name__=="__main__":
+    # for running in workflow
     df_train = pd.read_csv(PROCESSED_DATA_DIR / "train.csv")
 
     y_train = df_train.pop(target)
